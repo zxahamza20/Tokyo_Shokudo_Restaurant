3144fd90c2630eeeda1ec48199f5255e826bfdba
Joey Kim <joeykim999@gmail.com>
2026-04-27 00:56:47 -0500

    updated phone number under location page

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -38,7 +38,7 @@ export default function Location() {
             </div>
             <div className="info-block">
               <span className="section-tag">Contact</span>
-              <p>📞 (972) 555-0123</p>
+              <p>📞 (469) 626-0117</p>
               <p>✉️ plano@tokyoshokudo.com</p>
             </div>
           </div>
